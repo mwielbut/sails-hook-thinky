4c7b803ff10c8843bf4ab15109202389a64995c0
mwielbut <mwielbut@umich.edu>
2015-08-27 00:15:08 +0100

    better error capture / failure

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-/* global async, _ */
 
 /**
  * A hook to enable the Thinky ORM for RethinkDB in Sails. 
@@ -10,148 +9,156 @@
  * 
  */
 
-module.exports = function (sails)
+module.exports = function(sails)
 {
 
-    var Thinky = require('thinky');
-    var path = require('path');
-    var buildDictionary = require('sails-build-dictionary');
-    var _ = require('lodash');
+      var Thinky = require('thinky');
+      var path = require('path');
+      var buildDictionary = require('sails-build-dictionary');
+      var _ = require('lodash');
 
-    /**
-     * Hook definition
-     */
+      /**
+       * Hook definition
+       */
 
-    var hook = {
+      var hook = {
 
 
-        defaults:
-        {
-
-            globals:
+            defaults:
             {
-                thinky: true,
-                thinkymodels: true
+
+                  globals:
+                  {
+                        thinky: true,
+                        thinkymodels: true
+                  },
+
+                  // thinky specific configs
+                  thinky:
+                  {
+                        rethinkdb:
+                        {
+                              host: "localhost",
+                              port: 28015,
+                              authKey: "",
+                              db: "test"
+                        }
+                  }
             },
 
-            // thinky specific configs
-            thinky:
+            configure: function()
             {
-                rethinkdb:
-                {
-                    host: "localhost",
-                    port: 28015,
-                    authKey: "",
-                    db: "test"
-                }
-            }
-        },
+                  sails.once('lower', hook.teardown);
+            },
 
-        configure: function ()
-        {
-            sails.once('lower', hook.teardown);
-        },
-
-        initialize: function (cb)
-        {
-            var thinky = new Thinky(sails.config.thinky.rethinkdb);
-
-            // Expose modules on `sails`
-            sails.thinky = thinky;
-
-            // Expose globals (if enabled)
-            if (sails.config.globals.thinky)
-            {
-                global.thinky = thinky;
-            }
-
-            var thinkyDir = path.resolve(sails.config.appPath, 'api/thinky');
-
-
-            async.auto(
+            initialize: function(cb)
             {
 
-                // Load model and adapter definitions defined in the project
-                thinkyModels: function (next)
-                {
-                    buildDictionary.optional(
-                    {
-                        dirname: thinkyDir,
-                        filter: /(.+)\.(js)$/,
-                        depth: 1,
-                        caseSensitive: true
-                    }, next);
-                },
+                  var thinky = new Thinky(sails.config.thinky.rethinkdb);
 
-                bindToSails: ['thinkyModels', function (next, results)
-                {
-                    _.each(results.thinkyModels, function (modelDefinition)
-                    {
-                        // Add a reference to the Sails app that loaded the module
-                        modelDefinition.sails = sails;
-                        // Bind all methods to the module context
-                        _.bindAll(modelDefinition);
-                    });
-
-                    return next();
-                }],
-                build: ['bindToSails', function (next, results)
-                {
-                    _.each(results.thinkyModels, function eachInstantiatedModel(modelDefinition)
-                    {
-                        var modelId = modelDefinition.tableName || modelDefinition.globalId;
-                        var model = thinky.createModel(modelId, modelDefinition.schema, modelDefinition.options);
-
-                        // expose sails.thinkymodels[] in a similar fashion to sails.models[]
-                        sails.thinkymodels = sails.thinkymodels ||
-                        {};
-                        sails.thinkymodels[modelId] = model;
-
-                        // expose sails thinky models as globals in a similar fashion to waterline models
-                        if (sails.config.globals && sails.config.globals.thinkymodels)
+                  thinky.dbReady()
+                        .then(function()
                         {
-                            global[modelId] = model;
-                        }
-                    });
+                                    // Expose modules on `sails`
+                              sails.thinky = thinky;
 
-                    return next();
-                }],
-                initialize: ['build', function (next, results)
-                {
-                    // call the init funciton on each def to setup relationships
-                    _.each(results.thinkyModels, function eachInstantiatedModel(modelDefinition)
-                    {
-                        var modelId = modelDefinition.tableName || modelDefinition.globalId;
-                        var model = sails.thinkymodels[modelId]
+                              // Expose globals (if enabled)
+                              if (sails.config.globals.thinky)
+                              {
+                                    global.thinky = thinky;
+                              }
 
-                        modelDefinition.init(model);
-                    });
+                              var thinkyDir = path.resolve(sails.config.appPath, 'api/thinky');
 
-                    return next();
-                }],
+                              buildDictionary.optional(
+                              {
+                                    dirname: thinkyDir,
+                                    filter: /(.+)\.(js)$/,
+                                    depth: 1,
+                                    caseSensitive: true
+                              }, processModels);
 
-            }, cb);
+                        })
+                        .catch(function(error)
+                        {
+                              return cb(error);
+                        });
 
-        },
 
 
-        teardown: function (cb)
-        {
-            cb = cb || function (err)
+                  var processModels = function(error, thinkyModels)
+                  {
+                        if (error)
+                              return cb(error);
+
+
+                        _.each(thinkyModels, function(modelDefinition)
+                        {
+                              // Add a reference to the Sails app that loaded the module
+                              modelDefinition.sails = sails;
+                              // Bind all methods to the module context
+                              _.bindAll(modelDefinition);
+                        });
+
+                        _.each(thinkyModels, function eachInstantiatedModel(modelDefinition)
+                        {
+                              var modelId = modelDefinition.tableName || modelDefinition.globalId;
+                              var model = thinky.createModel(modelId, modelDefinition.schema, modelDefinition.options);
+
+                              // expose sails.thinkymodels[] in a similar fashion to sails.models[]
+                              sails.thinkymodels = sails.thinkymodels ||
+                              {};
+                              sails.thinkymodels[modelId] = model;
+
+                              // expose sails thinky models as globals in a similar fashion to waterline models
+                              if (sails.config.globals && sails.config.globals.thinkymodels)
+                              {
+                                    global[modelId] = model;
+                              }
+                        });
+
+                        // call the init funciton on each def to setup relationships
+                        _.each(thinkyModels, function eachInstantiatedModel(modelDefinition)
+                        {
+                              var modelId = modelDefinition.tableName || modelDefinition.globalId;
+                              var model = sails.thinkymodels[modelId];
+
+                              modelDefinition.init(model);
+                        });
+
+                        cb();
+
+
+                  };
+
+
+            },
+
+
+            teardown: function(cb)
             {
-                if (err)
-                {
-                    sails.log.error('Failed to teardown ORM hook.');
-                    sails.log.error(err);
-                }
-            };
+                  cb = cb || function(err)
+                  {
+                        if (err)
+                        {
+                              sails.log.error('Failed to teardown Thinky hook');
+                              sails.log.error(err);
+                        }
+                  };
 
-            sails.thinky.r.getPoolMaster().drain()
-                .catch(cb)
-                .finally(cb);
-        }
-    };
+                  if (sails.thinky)
+                  {
+                        sails.thinky.r.getPoolMaster().drain()
+                              .catch(cb)
+                              .finally(cb);
+                  }
+                  else
+                  {
+                        return cb(new Error("Thinky uninitialized."));
+                  }
+            }
+      };
 
-    return hook;
+      return hook;
 
 };
